0a6dc22791b8a77d24ad63a75bc9d4d301f73da4
Kyrylo Peretiatko <batontram@gmail.com>
2021-11-23 15:09:41 +0200

    function getLang(array $request, $default) v0.3.0

--- pub/index.php
+++ pub/index.php
@@ -5,16 +5,20 @@
  * лучше все данные передавать в нее через аргументы
  *
  * Более того это удобно для Unit тестирования
+ *
+ * Лучше передавать как можно меньше параметров в функцию.
+ * Для этого нужно предерживаться принципа единственной ответственности
+ * Еще один выход сгруппировать параметры. В данном случае в массив $request
  */
-function getLang(array $get, array $cookie, array $session, array $server, $default) {
-    if (!empty($get['lang'])) {
-        $lang = $get['lang'];
-    } elseif (!empty($cookie['lang'])) {
-        $lang = $cookie['lang'];
-    } elseif (!empty($session['lang'])) {
-        $lang = $session['lang'];
-    } elseif (!empty($server['lang'])) {
-        $lang = $server['lang'];
+function getLang(array $request, $default) {
+    if (!empty($request['get']['lang'])) {
+        $lang = $request['get']['lang'];
+    } elseif (!empty($request['cookie']['lang'])) {
+        $lang = $request['cookie']['lang'];
+    } elseif (!empty($request['session']['lang'])) {
+        $lang = $request['session']['lang'];
+    } elseif (!empty($request['server']['lang'])) {
+        $lang = $request['server']['lang'];
     } else {
         $lang = $default;
     }
@@ -26,6 +30,13 @@ session_start();
 $name = empty($_GET['name']) ? 'Guest' : $_GET['name'];
 header('X-Developer: Kyrylo');
 
-$lang = getLang($_GET, $_COOKIE, $_SESSION, $_SERVER, 'en');
+$request = [
+    'get' => $_GET,
+    'cookie' => $_COOKIE,
+    'session' => $_SESSION,
+    'server' => $_SERVER
+];
+
+$lang = getLang($request, 'en');
 
 echo "Hello, $name! Your lang is $lang";
