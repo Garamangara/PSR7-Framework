d44c076ce5f7a836eaeb071fe62e1a7f472221c3
Kyrylo Peretiatko <batontram@gmail.com>
2021-11-24 20:16:30 +0200

    improve response sender -> added my own ResponseSender class

--- pub/index.php
+++ pub/index.php
@@ -3,9 +3,8 @@ chdir(dirname(__DIR__));
 // прописать настройки по psr4 в секцию autoload в файле composer.json
 // Выполнить команду composer dump-autoload
 require 'vendor/autoload.php';
-//require __DIR__ . '/../vendor/autoload.php';
 
-use Framework\Http\RequestFactory;
+use Framework\Http\ResponseSender;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\ServerRequestFactory;
 
@@ -21,11 +20,8 @@ $response = (new HtmlResponse("Hello, $name!"))
     ->withHeader('X-Developer', 'Kyrylo');
 
 ### Sending
-header('HTTP/1.1 ' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
-foreach ($response->getHeaders() as $name => $value) {
-    header($name . ':' . $value);
-}
-echo $response->getBody();
+$sender = new ResponseSender();
+$sender->send($response);
 
 
 
